d2708f963a12b468cda5b33f13eb1a05a63a541b
qxforever <qxforeverr@qq.com>
2021-01-18 19:06:38 +0800

    optimize stack

--- main.cpp
+++ main.cpp
@@ -123,7 +123,7 @@ int main(int argc, char** argv) {
 		int a[5];
 		std::vector<instruction> w;
 		ins.get(a[0], a[1], a[2], a[3], a[4], w);
-		a[3] += 10;
+		a[3] += 1;
 		for (int i = 0; i < 5; i++) a[i] = little2big(a[i]), Write(&a[i], 4);
 		for (auto p : w) {
 			for (auto &c : p.key) c = tolower(c);
